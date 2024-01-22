a51fec6f074f9fa3bec0b620d078b9aadc12e31e
hedys9 <hedysadeghi@gmail.com>
2024-01-22 10:59:11 +0100

    edit recipe

--- src/main/java/org/learning/blogricette/controller/RecipeController.java
+++ src/main/java/org/learning/blogricette/controller/RecipeController.java
@@ -4,15 +4,15 @@ import jakarta.validation.Valid;
 import org.learning.blogricette.model.Recipe;
 import org.learning.blogricette.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/recipes")
@@ -39,12 +39,42 @@ public class RecipeController {
 
 
     @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("recipe") Recipe recipeForm, BindingResult bindingResult, Model model) {
+    public String store(@Valid @ModelAttribute("recipe") Recipe recipeForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "recipes/create";
         }
         Recipe savedRecipe = recipeRepository.save(recipeForm);
         return "redirect:/recipes/list";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Recipe> result = recipeRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("recipe", result.get());
+            return "recipes/edit";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe with id " + id + " not found");
+        }
+
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("recipe") Recipe recipeForm, BindingResult bindingResult) {
+        Optional<Recipe> result = recipeRepository.findById(id);
+        if (result.isPresent()) {
+
+            if (bindingResult.hasErrors()) {
+                return "recipes/edit";
+            }
+
+            Recipe updatedRecipe = recipeRepository.save(recipeForm);
+            return "redirect:/recipes/list";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe with id " + id + " not found");
+
+        }
     }
 
 
